09f135e312839e6f06a0c7b832c6051ae8dbdeb7
Cat <95453766+Catbreadisme@users.noreply.github.com>
2024-08-01 12:51:19 +1200

    Update DrawPalette.java

--- DrawPalette.java
+++ DrawPalette.java
@@ -2,6 +2,14 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
 
+/*
+    By:         Cat Bowden
+    What:       Image To Colour Palette Program
+    Why:        Computer Science Assesment AS91906
+    When:       1/08/2024
+    Version:    12
+*/
+
 public class DrawPalette extends JPanel {
     ArrayList<String> palette;
 
